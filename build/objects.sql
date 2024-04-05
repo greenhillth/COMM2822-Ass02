037991a62556c0be8f99599bffadb1ca0033f3cf
Tom Greenhill <79777560+greenhillth@users.noreply.github.com>
2024-04-05 20:25:20 +1100

    first tables built

--- build/objects.sql
+++ build/objects.sql
@@ -1,8 +1,15 @@
-CREATE
-OR REPLACE TYPE ADDR_OBJ AS OBJECT (
+CREATE OR REPLACE TYPE ADDR_OBJ AS OBJECT (
     num NUMBER(4, 0),
     street VARCHAR2(100),
     city VARCHAR2(50),
     state VARCHAR2(3),
     postcode NUMBER(4, 0)
+);
+
+CREATE OR REPLACE TYPE CONTACT AS OBJECT (
+    name_first VARCHAR2(25),
+    name_last VARCHAR2(25),
+    phone_number VARCHAR2(25),
+    address ADDR_OBJ,
+    email VARCHAR2(20)
 );
